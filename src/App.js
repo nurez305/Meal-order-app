67769969206d4ed2c74923e936a0118719f8dec6
nurez305 <nurudeen.saliu@decagon.dev>
2022-12-08 18:39:14 +0100

    adding meal list with dummy data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import {Fragment} from 'react'
 import Header from './component/Layout/Header';
+import Meals from './component/Meals/Meals'
 
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
