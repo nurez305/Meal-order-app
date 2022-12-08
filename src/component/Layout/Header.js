bfc5064535c96db0a897586764443ba8c2a07e11
nurez305 <nurudeen.saliu@decagon.dev>
2022-12-08 17:46:15 +0100

    cart-button design

--- src/component/Layout/Header.js
+++ src/component/Layout/Header.js
@@ -1,13 +1,14 @@
 import {Fragment} from 'react'
 import classes from './Header.module.css'
 import BgImage from '../../Asset/meals.jpg'
+import HeaderCartButton from './HeaderCartButton'
 
 function Header() {
   return (
     <Fragment>
         <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
         </header>
         <div className={classes['main-image']}>
         <img src={BgImage} alt="banner bg"/>
