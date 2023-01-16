2257589f8f642e00c20804c81eff6c1ee1858ddb
nurez305 <nurudeen.saliu@decagon.dev>
2023-01-16 12:40:47 +0100

    added new loading spinner

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,108 +1,115 @@
-import React from 'react'
-import { useState } from 'react'
-import { Link, useNavigate} from 'react-router-dom'
-import axios from '../../Api/axios'
-import classes from './Login.module.css'
-import {toast} from 'react-toastify'
+import React from "react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "../../Api/axios";
+import classes from "./Login.module.css";
+import { toast } from "react-toastify";
 
 const Login = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [loading, setLoading] = useState(false)
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
-    if(!email || password.trim().length === 0 ){
-      return toast("email and password must not be empty")
+    e.preventDefault();
+    if (!email || password.trim().length === 0) {
+      return toast("email and password must not be empty");
     }
-    setLoading(true)
-    try { 
-      const response = await axios.post('/login', {
+    setLoading(true);
+    try {
+      const response = await axios.post("/login", {
         email,
         password,
-      })
+      });
       // console.log(response)
-      localStorage.setItem('token', response.data.token)
-      toast(<div>{`Welcome ${response.data.name}`}</div>)
-      navigate("/store")
-      setLoading(false)
+      localStorage.setItem("token", response.data.token);
+      toast(<div>{`Welcome ${response.data.name}`}</div>);
+      navigate("/store");
+      setLoading(false);
     } catch (error) {
-      toast(<div>{error.response.data.message}</div>)
-        setLoading(false)
+      toast(<div>{error.response.data.message}</div>);
+      setLoading(false);
     }
-  }
+  };
 
   const handleEmail = (e) => {
-    setEmail(e.target.value)
-    
-  }
+    setEmail(e.target.value);
+  };
 
   const handlePassword = (e) => {
-    setPassword(e.target.value)
-    
-  }
+    setPassword(e.target.value);
+  };
 
   return (
-  <>
-{loading && (
-    <div className={classes.loadingmodal}>
-      <div className={classes.loading}></div>
-      </div>
+    <>
+      {loading && (
+        // <div className={classes.loadingmodal}>
+        //   <div className={classes.loading}></div>
+        //   </div>
+
+        <div className={classes["lds-spinner"]}>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
       )}
 
-<form onSubmit={handleSubmit} className={classes["form_container"]}>
-<div className={classes["form_head"]}>  
-<h2>Naija Meals</h2>
-</div>
-<div className={classes.container}>
-<div className={classes['form_input']}>
-  <label htmlFor='email'>Email</label>
-  <input
-    type='email'
-    autoComplete='off'
-    placeholder='Enter your email'
-    onChange={handleEmail}
-    value={email}
-    // required
-  />
-</div>
+      <form onSubmit={handleSubmit} className={classes["form_container"]}>
+        <div className={classes["form_head"]}>
+          <h2>Naija Meals</h2>
+        </div>
+        <div className={classes.container}>
+          <div className={classes["form_input"]}>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              autoComplete="off"
+              placeholder="Enter your email"
+              onChange={handleEmail}
+              value={email}
+              // required
+            />
+          </div>
 
-<div className={classes["form_input"]}>
-  <label htmlFor=''>Password</label>
-  <input
-    type='password'
-    autoComplete='off'
-    placeholder='Enter your password'
-    onChange={handlePassword}
-    value={password}
-    // required
-  />
-</div>
-
-</div>
-{/* <div className={classes['login_forgot']}>
+          <div className={classes["form_input"]}>
+            <label htmlFor="">Password</label>
+            <input
+              type="password"
+              autoComplete="off"
+              placeholder="Enter your password"
+              onChange={handlePassword}
+              value={password}
+              // required
+            />
+          </div>
+        </div>
+        {/* <div className={classes['login_forgot']}>
   <Link to='/' className={classes.forgot}>Forgot password?</Link>
 </div> */}
 
-<button
-  type='submit'
-  className={classes['login_btn']}
->
-  Sign In
-</button>
-<p className={classes['login_text']}>
-  Dont have an account?{' '}
-  <Link to='/register' className={classes['login_anchor']}>
-    Create Account
-  </Link>
-</p>
-</form>
-  
-   </>
-  )
+        <button type="submit" className={classes["login_btn"]}>
+          Sign In
+        </button>
+        <p className={classes["login_text"]}>
+          Dont have an account?{" "}
+          <Link to="/register" className={classes["login_anchor"]}>
+            Create Account
+          </Link>
+        </p>
+      </form>
+    </>
+  );
+};
 
-}
-
-export default Login
+export default Login;
